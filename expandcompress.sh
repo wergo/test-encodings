3fc0e540712efbcf74eecbb63e271a890dd22681
Werner Goebl <goebl@mdw.ac.at>
2025-02-27 17:11:17 +0100

    Expand three expansions with patched Verovio

---
+++ expandcompress.sh
@@ -0,0 +1,3 @@
+verovio -a -t mei --expand minimal -o ExpandCompressExample-minimal.mei ExpandCompressExample.mei
+verovio -a -t mei --expand default -o ExpandCompressExample-default.mei ExpandCompressExample.mei
+verovio -a -t mei --expand unsorted -o ExpandCompressExample-unsorted.mei ExpandCompressExample.mei
