b5c74209ede03b6a5f986592847b152c8a3efc9f
Werner Goebl <goebl@mdw.ac.at>
2025-09-02 15:41:47 +0200

    Add expand shell script

---
+++ expandcompress-currentVerovio.sh
@@ -0,0 +1,3 @@
+verovio -a -t mei --expand minimal -o ExpandCompressExample-minimal-curr.mei ExpandCompressExample.mei
+verovio -a -t mei --expand default -o ExpandCompressExample-default-curr.mei ExpandCompressExample.mei
+verovio -a -t mei --expand unsorted -o ExpandCompressExample-unsorted-curr.mei ExpandCompressExample.mei
