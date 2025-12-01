7d4bfe66032bbaddfa388266ebe49eca3b35e8e3
Werner Goebl <goebl@mdw.ac.at>
2025-12-01 16:55:19 +0100

    Add expansions to Schumann von fremden Landern

---
+++ expand-Schumann.sh
@@ -0,0 +1,2 @@
+verovio -a -t mei --expand expansion-minimal -o Schumann-Von-fremden-Landern-und-Menschen-minimal.mei Schumann-Von-fremden-Landern-und-Menschen.mei
+verovio -a -t mei --expand expansion-default -o Schumann-Von-fremden-Landern-und-Menschen-default.mei Schumann-Von-fremden-Landern-und-Menschen.mei
